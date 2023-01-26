af2ce8e668b881ea1b11b7ca2dc5e543a0a0cc8c
Prechh <adamadrien21@gmail.com>
2023-01-26 16:03:05 +0100

    Ajout formulaire de connexion et page accueil

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_login')]
-    public function index(): Response
+    #[Route('/', name: 'app_login',  methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('login/login.html.twig', [
-            'controller_name' => 'LoginController',
+            'error' => $authenticationUtils->getLastAuthenticationError()
         ]);
+    }
+    #[Route('/logout', 'app_login_logout')]
+    public function logout()
+    {
+        // Nothing to do here..
     }
 }
